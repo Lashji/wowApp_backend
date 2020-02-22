031986b2cd80ce883e56996a01af652042a89dda
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2020-02-23 00:56:18 +0200

    downloading class icons works

--- utils/mediaHandler.js
+++ utils/mediaHandler.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const https = require('https')
+const path = require('path')
+const axios = require('axios')
+
 class MediaHandler {
 
 	constructor(request) {
@@ -5,6 +10,7 @@ class MediaHandler {
 		this.init()
 		this.classIcons = []
 		this.specIcons = []
+		this.index = []
 	}
 
 	init() {
@@ -19,20 +25,58 @@ class MediaHandler {
 
 	async fetchClassIcons() {
 		console.log("fetching classIcons")
-		let icons = []
-		for (let i = 1; i <= 12; i++) {
-			let url = `https://us.api.blizzard.com/data/wow/media/playable-class/${i}?namespace=static-us&locale=en_US`
-			this.doRequest(url)
-				.then(res => {
-					console.log("res", res.data)
-					icons.push(res.data)
-				})
+		await this.getIndex()
+		let classes = this.index.classes
+
+		for (let i = 0; i < classes.length; i++) {
+			let url = `https://us.api.blizzard.com/data/wow/media/playable-class/${classes[i].id}?namespace=static-us&locale=en_US`
+			let response = await this.doRequest(url)
+			let ic = response.data.assets[0].value
+			// let data = await this.doRequest(ic)
+			console.log("ic", ic)
+			// console.log("data", data)
+			this.saveImage(`classIcons/${classes[i].name}.jpg`, ic)
+			// this.saveImageAxios(`classIcons/${classes[i].name}.jpg`, ic)
 		}
 
-		// Promise.all(icons)
-		// .then(res => console.log("resss", res))
-		// console.log("classIcons", this.classIcons)
+	}
 
+	async getIndex() {
+		const url = "https://us.api.blizzard.com/data/wow/playable-class/index?namespace=static-us&locale=en_US"
+		let res = await this.doRequest(url)
+		this.index = res.data
+	}
+
+
+	// async saveImage(path, url) {
+	// 	try {
+	// 		let fullPath = `${__dirname}/../public/images/${path}`
+	// 		https.get(url, (res) => {
+	// 			res.pipe(fs.createWriteStream(fullPath))
+	// 		})
+
+	// 	} catch (err) {
+	// 		console.log("error when trying to save file", err)
+	// 	}
+	// }
+
+	async saveImage(path, url) {
+		let fullPath = `${__dirname}/../public/images/${path}`
+
+		const writer = fs.createWriteStream(fullPath)
+
+		const response = await axios({
+			url,
+			method: 'get',
+			responseType: 'stream'
+		})
+
+		response.data.pipe(writer)
+
+		return new Promise((resolve, reject) => {
+			writer.on('finish', resolve)
+			writer.on('error', reject)
+		})
 	}
 }
 
