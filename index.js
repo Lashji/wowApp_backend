200391186d049fd8d72df2e78ce090a15c0d0785
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2020-02-23 23:50:47 +0200

    serving static images

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ db.connectDB(dbConfig)
 
 app.use(helmet())
 app.use(express.json())
-
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", '*')
